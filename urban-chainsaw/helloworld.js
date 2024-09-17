37fffbf561c4510538a6eb5945f36fcfaa7641df
Arthur Nagy Tibes <160873696+DeusAbsoluto666@users.noreply.github.com>
2024-09-17 12:09:45 -0300

    Java01

---
+++ urban-chainsaw/helloworld.js
@@ -0,0 +1,15 @@
+    //HelloWorld
+
+const number01 = 50
+const number02 = 70
+const number03 = 90
+
+console.log(soma)
+console.log(subtracao)
+console.log(divisao)
+console.log(multiplicacao)
+
+const soma = number01 + number02
+const subtracao = number01 - number02
+const divisao = number02 / number01
+const multiplicacao = number01 * number03
